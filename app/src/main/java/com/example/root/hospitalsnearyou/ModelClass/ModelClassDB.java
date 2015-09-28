b6fc0f54dc039ab27bd04a197d4d44e4e4b328be
rahul7977 <rahul.kamble827@gmail.com>
2015-09-28 16:01:14 +0530

    new

--- app/src/main/java/com/example/root/hospitalsnearyou/ModelClass/ModelClassDB.java
+++ app/src/main/java/com/example/root/hospitalsnearyou/ModelClass/ModelClassDB.java
@@ -4,4 +4,169 @@ package com.example.root.hospitalsnearyou.ModelClass;
  * Created by root on 28/9/15.
  */
 public class ModelClassDB {
+    private String hospitalId;
+    private String state;
+    private String city;
+    private String district;
+    private String hospitalName;
+    private String address;
+    private String pincode;
+    private String contact;
+    private String helpline;
+    private String fax;
+    private String category;
+    private String website;
+    private String email;
+    private String blood_component;
+    private String blood_group;
+    private String service_time;
+    private String latitude;
+    private String longitude;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBlood_component() {
+        return blood_component;
+    }
+
+    public void setBlood_component(String blood_component) {
+        this.blood_component = blood_component;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getHelpline() {
+        return helpline;
+    }
+
+    public void setHelpline(String helpline) {
+        this.helpline = helpline;
+    }
+
+    public String getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(String hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getService_time() {
+        return service_time;
+    }
+
+    public void setService_time(String service_time) {
+        this.service_time = service_time;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 }
+
+
+
