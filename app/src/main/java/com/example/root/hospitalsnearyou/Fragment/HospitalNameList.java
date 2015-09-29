05b5faa405d976aa01bc9725765ef14217657658
shekharshende <shekhar.shende@synerzip.com>
2015-09-29 15:04:48 +0530

    ahe

--- app/src/main/java/com/example/root/hospitalsnearyou/Fragment/HospitalNameList.java
+++ app/src/main/java/com/example/root/hospitalsnearyou/Fragment/HospitalNameList.java
@@ -1,8 +1,25 @@
 package com.example.root.hospitalsnearyou.Fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.root.hospitalsnearyou.R;
 
 public class HospitalNameList extends Fragment {
+    public  HospitalNameList()
+    {
 
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_hospital_name_list, container, false);
+
+        return rootView;
+    }
 
 }
